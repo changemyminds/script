7b86d512ee66672f2f667b25d165e13f13bac46c
changemyminds <david7717318@gmail.com>
2020-10-27 15:17:16 +0800

    add install sh

---
+++ install/install_ubuntu.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+# 更新apt安裝
+sudo apt-get update
+
+# 安裝相關packages
+sudo apt-get install apt-transport-https ca-certificates curl software-properties-common
+
+# 新增 Docker 官方 GPG key
+sudo curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
+
+# 新增 stable 版本 repository.
+sudo add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable"
+
+# 安裝docker-ce、docker-ce-cli、containerd.io
+sudo apt-get install docker-ce docker-ce-cli containerd.io
+
+# 將目前使用者添加到docker環境內，這邊也可以直接將${USER}進行取代，另如usermod -aG docker darren
+sudo usermod -aG docker ${USER}
