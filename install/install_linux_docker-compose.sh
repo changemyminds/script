f64e4e917de02899b06c86a38f1fdfa1b9021055
changemyminds <david7717318@gmail.com>
2020-12-28 21:17:05 +0800

    added prometheus and grafana docker-compose

---
+++ install/install_linux_docker-compose.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+# 安裝docker-compose，下載docker compose
+sudo curl -L "https://github.com/docker/compose/releases/download/1.27.4/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
+
+# 查看安裝路徑
+ls -al /usr/local/bin/docker-compose
+
+# 改變權限，讓docker-compose變為可以執行
+sudo chmod +x /usr/local/bin/docker-compose
+
+# 查看修改完權限後的結果
+ls -al /usr/local/bin/docker-compose
+
+# 測試docker-compose指令
+docker-compose -v
