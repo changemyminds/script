9c060cad8d128172cf8adcb0d97cecb46741eddf
changemyminds <david7717318@gmail.com>
2022-07-20 00:37:15 +0800

    fix: 修正版本

--- docker/install/ubuntu/install-ubuntu2004-latest.sh
+++ docker/install/ubuntu/install-ubuntu2004-latest.sh
@@ -1,17 +1,15 @@
 #!/bin/bash
 
-export DOCKER_VERSION=5:19.03.15~3-0~ubuntu-focal
-export CONTAINERD_IO_VERSION=1.6.6-1
-export DOCKER_COMPOSE_VERSION=2.5.0~ubuntu-focal
-
 sudo apt-get update
-sudo apt-get install -y ca-certificates curl software-properties-common
+sudo apt-get install -y apt-transport-https ca-certificates curl software-properties-common
 sudo mkdir -p /etc/apt/keyrings
 sudo curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo gpg --dearmor -o /etc/apt/keyrings/docker.gpg
 echo \
   "deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/docker.gpg] https://download.docker.com/linux/ubuntu \
   $(lsb_release -cs) stable" | sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
 sudo apt-get update
-sudo apt-get install -y docker-ce=${DOCKER_VERSION} docker-ce-cli:amd64=${DOCKER_VERSION} containerd.io=${CONTAINERD_IO_VERSION} docker-compose-plugin=${DOCKER_COMPOSE_VERSION}
+sudo apt-get install -y docker-ce docker-ce-cli containerd.io docker-compose-plugin
 sudo usermod -aG docker ${USER}
 sudo service docker start
+
+
