4fecda9c7a270f236c5eb9c837e1a7e53055160a
changemyminds <david7717318@gmail.com>
2022-07-20 00:31:10 +0800

    fix: fixed ubuntu 20.40 amd64 version

--- docker/install/ubuntu/install-ubuntu2004-19.x-amd64.sh
+++ docker/install/ubuntu/install-ubuntu2004-19.x-amd64.sh
@@ -6,8 +6,11 @@ export DOCKER_COMPOSE_VERSION=2.5.0~ubuntu-focal
 
 sudo apt-get update
 sudo apt-get install -y ca-certificates curl software-properties-common
-sudo curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
-sudo add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable"
+sudo mkdir -p /etc/apt/keyrings
+sudo curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo gpg --dearmor -o /etc/apt/keyrings/docker.gpg
+echo \
+  "deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/docker.gpg] https://download.docker.com/linux/ubuntu \
+  $(lsb_release -cs) stable" | sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
 sudo apt-get update
 sudo apt-get install -y docker-ce=${DOCKER_VERSION} docker-ce-cli:amd64=${DOCKER_VERSION} containerd.io=${CONTAINERD_IO_VERSION} docker-compose-plugin=${DOCKER_COMPOSE_VERSION}
 sudo usermod -aG docker ${USER}
