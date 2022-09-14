5c8e16895df286193ccbc74aa3f76caeda92a7c2
changemyminds <david7717318@gmail.com>
2022-09-14 10:07:19 +0800

    feat: add clean rancher

---
+++ kubernetes/clean/rancher/clearn-rancher.sh
@@ -0,0 +1,24 @@
+#!/bin/bash -eux
+
+docker stop $(docker ps -aq)
+docker system prune -f
+docker volume rm $(docker volume ls -q)
+docker image rm $(docker image ls -q)
+
+sudo rm -rf /etc/ceph \
+       /etc/cni \
+       /etc/kubernetes \
+       /opt/cni \
+       /opt/rke \
+       /run/secrets/kubernetes.io \
+       /run/calico \
+       /run/flannel \
+       /var/lib/calico \
+       /var/lib/etcd \
+       /var/lib/cni \
+       /var/lib/kubelet \
+       /var/lib/rancher/rke/log \
+       /var/log/containers \
+       /var/log/pods \
+       /var/run/calico
+       
