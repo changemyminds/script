9f73ba2441bdca21b5faf890d9d25d3a431c1ffe
changemyminds <changemyminds>
2023-08-18 23:12:11 +0800

    fix: update k8s repo url

--- vagrant/install-scripts/install_kubectl.sh
+++ vagrant/install-scripts/install_kubectl.sh
@@ -9,7 +9,7 @@ sudo sed -i 's/in\./us\./g' /etc/apt/sources.list
 # add kubernetes repo  
 sudo apt-get update
 sudo apt-get install -y apt-transport-https ca-certificates curl
-sudo curl -fsSLo /usr/share/keyrings/kubernetes-archive-keyring.gpg https://packages.cloud.google.com/apt/doc/apt-key.gpg
+sudo curl -fsSLo /usr/share/keyrings/kubernetes-archive-keyring.gpg https://dl.k8s.io/apt/doc/apt-key.gpg
 echo "deb [signed-by=/usr/share/keyrings/kubernetes-archive-keyring.gpg] https://apt.kubernetes.io/ kubernetes-xenial main" | sudo tee /etc/apt/sources.list.d/kubernetes.list
 
 # install kubectl
